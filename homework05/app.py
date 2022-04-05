cfad991cc6025e045df4dc2bb0d7dfea6260613e
Anneris Rodriguez <annerisrod@utexas.edu>
2022-04-05 10:12:21 -0500

    uploaded the files for homework05

---
+++ homework05/app.py
@@ -0,0 +1,44 @@
+from flask import Flask, request
+import json
+import redis
+
+
+app = Flask(__name__)
+
+@app.route('/data', methods = ['GET', 'POST'])
+def data(search_start = 0):
+    '''
+    
+    '''
+    if(request.method == 'POST'):
+        counter = 0
+        #load data into redis database
+        data_dict = json.load(open('ML_Data_Sample.json','r'))
+        data_list = data_dict['meteorite_landings']
+        for d in data_list:
+            data = data_list[d]
+            rd.hset(str(counter), mapping={'name': data['name'],\
+                    'id': data['id'], 'recclass': data['recclass'],\
+                    'mass (g)': data['mass (g)'], 'reclat': data['reclat'],\
+                    'reclong': data['reclong'], 'GeoLocation': data['GeoLocation']})
+            counter += 1
+        return 'Meteorite Landing data uploaded.'
+
+    elif(request.method == 'GET'):
+        # read data out of redis
+        # return data as a json list
+        # BONUS: add start query parameter for value at which to start return from
+        return_data = []
+        for key,value in rd.hscan_iter():
+            return_data.append(json.loads(value.decode('utf-8')))
+        return return_data
+            
+    else:
+        # return error message with the correct methods to use
+        return '    The method given does not exist.\n        To update\
+                the data, please use "POST".\n        To retrieve data,\
+                please use "GET".'
+
+if __name__ == '__main__':
+    rd = redis.Redis(host='172.17.0.19', port='6379')
+    app.run(debug=True, host='0.0.0.0')
